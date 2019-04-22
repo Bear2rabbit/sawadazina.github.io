0876b9a4b741ef7da261d26e2947a86d9529d6d5
刚 <2316650427@qq.com>
2019-04-22 14:09:32 +0800

    Update main.js

--- src/Funny-demo/musicPlayer/main.js
+++ src/Funny-demo/musicPlayer/main.js
@@ -1,4 +1,51 @@
 (function() {
+  //对音频buffer进行解码
+  function decodeBuffer(arraybuffer, callback) {
+    AC.decodeAudioData(arraybuffer).then(function (buffer) {
+      callback(buffer);
+    }, function (e) {
+      alert("文件解码失败")
+    })
+  }
+  //音频播放
+  function playMusic(arg) {
+    var source;
+    //如果arg是audio的dom对象，则转为相应的源
+    if (arg.nodeType) {
+      audioSource = audioSource || AC.createMediaElementSource(arg);
+      source = audioSource;
+    } else {
+      bufferSource = AC.createBufferSource();
+
+      bufferSource.buffer = arg;
+
+      bufferSource.onended = function () {
+        app.trigger(singleLoop ? nowIndex : (nowIndex + 1));
+      };
+
+      //播放音频
+      setTimeout(function () {
+        bufferSource.start()
+      }, 0);
+
+      source = bufferSource;
+    }
+
+    //连接analyserNode
+    source.connect(analyser);
+
+    //再连接到gainNode
+    analyser.connect(gainnode);
+
+    //最终输出到音频播放器
+    gainnode.connect(AC.destination);
+  }
+  //选择audio作为播放源
+  function chooseMusic(src) {
+    audio.src = src;
+    audio.load();
+    playMusic(audio);
+  }
   var canvas = document.getElementById("cas");
   var ctx = canvas.getContext("2d");
   var outcanvas = document.createElement("canvas");
@@ -6,6 +53,8 @@
   outcanvas.height = canvas.height / 2;
   var octx = outcanvas.getContext('2d');
   // audioSource 为音频源，bufferSource为buffer源
+  // 读取到的音频文件时2进制文件（可以使用Ajax远程加载，可以使用FileAPI读取本地文件），我们需要让AudioContext先对其解码，然后再进行后续擦做。
+  // 返回结果的buffer为AudioBuffer类型数据
   var audioSource, bufferSource;
 
   //实例化音频对象
@@ -23,15 +72,13 @@
 
   // gain为gainNode，音频的声音处理模块
   var gainnode = AC.createGain();
-  gainnode.gain.value = 1;
+  gainnode.gain.value = 1;//改变gainnode.gain.value来实现静音
 
   //计时器
   var RAF = (function() {
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || 
     window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
      function(callback) {
-       console.log(callback);
-       
        window.setTimeout(callback, 1000 / 60);};
   })();
 
@@ -41,13 +88,11 @@
   var musics = [{
     name: "Fate Stay Night",
     src: "music2.mp3"
-  }, {
-    name: "Two Steps From Hell",
-    src: "music.mp3"
   }];
   var nowIndex = 0;   //当前播放到的音乐索引
   var singleLoop = false; //是否单曲循环
 
+  // 渲染播放列表定义方法，绑定事件
   var app = {
     init: function() {
       this.render(musics);
@@ -59,7 +104,6 @@
 
     bind: function() {
       var that = this;
-
       audio.onended = function() {
         app.trigger(singleLoop ? nowIndex : (nowIndex + 1));
       };
@@ -87,13 +131,16 @@
 
       //如果用户选取了自己的音乐则通过filereader读取
       $('.music-file').on('change', function() {
+        console.log('ddddd');
+        
         if (this.files.length == 0) return;
-
+        console.log(this.files);
+          
         var files = Array.prototype.slice.call(this.files);
-
+        console.log(files);
         files.forEach(function(file) {
           var fr = new FileReader();
-          fr.readAsArrayBuffer(file);
+          fr.readAsArrayBuffer(file);//按字节读取文件内容，结果用ArrayBuffer对象表示
           var mdata = {
             name: file.name.substring(0, file.name.lastIndexOf('.')),
             buffer: null,
@@ -102,7 +149,7 @@
 
           musics.push(mdata);
 
-          fr.onload = function(e) {
+          fr.onload = function(e) {//加载成功onload
             decodeBuffer(e.target.result, function(buffer) {
               mdata.buffer = buffer;
               mdata.decoding = false;
@@ -118,7 +165,7 @@
     trigger: function(index) {
       index = index >= musics.length ? 0 : index;
 
-      if (musics[index].decoding)return;
+       if (musics[index].decoding)return;
 
       this.stop();
 
@@ -141,8 +188,9 @@
 
       if (!audio.ended || !audio.paused) audio.pause();
 
-      if (bufferSource && ('stop' in bufferSource)) bufferSource.stop();
-
+      if (bufferSource && ('stop' in bufferSource)){
+          bufferSource.stop()
+      } 
       try {
         if (bufferSource) {
           bufferSource.disconnect(analyser);
@@ -173,56 +221,9 @@
     }
   };
 
-  //选择audio作为播放源
-  function chooseMusic(src) {
-    audio.src = src;
-    audio.load();
-    playMusic(audio);
-  }
+  
 
-  //对音频buffer进行解码
-  function decodeBuffer(arraybuffer, callback) {
-    AC.decodeAudioData(arraybuffer, function(buffer) {
-      callback(buffer);
-    }, function(e) {
-      alert("文件解码失败")
-    })
-  }
-
-  //音频播放
-  function playMusic(arg) {
-    var source;
-    //如果arg是audio的dom对象，则转为相应的源
-    if (arg.nodeType) {
-      audioSource = audioSource || AC.createMediaElementSource(arg);
-      source = audioSource;
-    } else {
-      bufferSource = AC.createBufferSource();
-
-      bufferSource.buffer = arg;
-
-      bufferSource.onended = function() {
-        app.trigger(singleLoop ? nowIndex : (nowIndex + 1));
-      };
-
-      //播放音频
-      setTimeout(function() {
-        bufferSource.start()
-      }, 0);
-
-      source = bufferSource;
-    }
-
-    //连接analyserNode
-    source.connect(analyser);
-
-    //再连接到gainNode
-    analyser.connect(gainnode);
-
-    //最终输出到音频播放器
-    gainnode.connect(AC.destination);
-  }
-
+  
   //绘制音谱的参数
   var rt_array = [],	//用于存储柱形条对象
       rt_length = 30;		//规定有多少个柱形条
@@ -231,6 +232,7 @@
   grd.addColorStop(0.3, "yellow");
   grd.addColorStop(1, "#00E800");
 
+  // 画布文字属性
   function showTxt(msg) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
@@ -241,77 +243,19 @@
     ctx.fillText(msg, canvas.width / 2, canvas.height / 2);
     ctx.restore();
   }
-
-  //动画初始化，获取analyserNode里的音频buffer
-  function initAnimation() {
-    //每个柱形条的宽度，及柱形条宽度+间隔
-    var aw = canvas.width / rt_length;
-    var w = aw - 5;
-
-    for (var i = 0; i < rt_length; i++) {
-      rt_array.push(new Retangle(w, 5, i * aw, canvas.height / 2))
-    }
-
-    animate();
-  }
-
-  function animate() {
-    if (!musics[nowIndex].decoding) {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      octx.clearRect(0, 0, canvas.width, canvas.height);
-
-      //出来的数组为8bit整型数组，即值为0~256，整个数组长度为1024，即会有1024个频率，只需要取部分进行显示
-      var array_length = analyser.frequencyBinCount;
-      var array = new Uint8Array(array_length);
-      analyser.getByteFrequencyData(array);	//将音频节点的数据拷贝到Uin8Array中
-
-      //数组长度与画布宽度比例
-      var bili = array_length / canvas.width;
-
-      for (var i = 0; i < rt_array.length; i++) {
-        var rt = rt_array[i];
-        //根据比例计算应该获取第几个频率值，并且缓存起来减少计算
-        rt.index = ('index' in rt) ? rt.index : ~~(rt.x * bili);
-        rt.update(array[rt.index]);
-      }
-
-      draw();
-    } else {
-      showTxt("音频解码中...")
-    }
-    console.log(RAF);
-    
-    RAF(animate);
-  }
-
-  //制造半透明投影
-  function draw() {
-    ctx.drawImage(outcanvas, 0, 0);
-    ctx.save();
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(Math.PI);
-    ctx.scale(-1, 1);
-    ctx.drawImage(outcanvas, -canvas.width / 2, -canvas.height / 2);
-    ctx.restore();
-    ctx.fillStyle = 'rgba(0, 0, 0, .8)';
-    ctx.fillRect(0, canvas.height / 2, canvas.width, canvas.height / 2);
-  }
-
   // 音谱条对象
   function Retangle(w, h, x, y) {
-    this.w = w;
+    this.w = w; // 小红块宽度
     this.h = h; // 小红块高度
-    this.x = x;
+    this.x = x; 
     this.y = y;
     this.jg = 3;
     this.power = 0;
     this.dy = y; // 小红块位置
     this.num = 0;
   }
-
-  var Rp = Retangle.prototype;
-
-  Rp.update = function(power) {
+  
+  Retangle.prototype.update = function (power) {
     this.power = power;
     this.num = ~~(this.power / this.h + 0.5);
 
@@ -328,7 +272,7 @@
     this.draw();
   };
 
-  Rp.draw = function() {
+  Retangle.prototype.draw = function () {
     octx.fillStyle = grd;
     var h = (~~(this.power / (this.h + this.jg))) * (this.h + this.jg);
     octx.fillRect(this.x, this.y - h, this.w, h);
@@ -339,7 +283,66 @@
     octx.fillStyle = "#950000";
     octx.fillRect(this.x, ~~this.dy, this.w, this.h);
   };
+  //动画初始化，获取analyserNode里的音频buffer
+  function initAnimation() {
+    //每个柱形条的宽度，及柱形条宽度+间隔
+    var aw = canvas.width / rt_length;
+    var w = aw - 5;
+
+    for (var i = 0; i < rt_length; i++) {
+      rt_array.push(new Retangle(w, 5, i * aw, canvas.height / 2))
+    }
+    console.log(rt_array);
+    animate();
+  }
+
+  function animate() {
+    if (!musics[nowIndex].decoding) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      octx.clearRect(0, 0, canvas.width, canvas.height);
+
+      //出来的数组为8bit整型数组，即值为0~256，整个数组长度为1024，即会有1024个频率，只需要取部分进行显示
+      var array_length = analyser.frequencyBinCount;
+      var array = new Uint8Array(array_length);
+      analyser.getByteFrequencyData(array);	//将音频节点的数据拷贝到Uint8Array中
+
+      //数组长度与画布宽度比例
+      var bili = array_length / canvas.width;
+
+      for (var i = 0; i < rt_array.length; i++) {
+        var rt = rt_array[i];
+        //根据比例计算应该获取第几个频率值，并且缓存起来减少计算
+        rt.index = ('index' in rt) ? rt.index : ~~(rt.x * bili);//~~运算符将变量转化为Number类型
+        console.log(rt , ~~(rt.x * bili));
+        
+        rt.update(array[rt.index]);
+      }
+
+      draw();
+    } else {
+      showTxt("音频解码中...")
+    }
+    // console.log(RAF);
+    
+    RAF(animate);
+  }
+
+  //制造半透明投影
+  function draw() {
+    ctx.drawImage(outcanvas, 0, 0);
+    // 绘制音频条下半部分开始
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    ctx.rotate(Math.PI);
+    ctx.scale(-1, 1);
+    ctx.drawImage(outcanvas, -canvas.width / 2, -canvas.height / 2);
+    ctx.restore();
+     // 绘制音频条下半部分结束
+    ctx.fillStyle = 'rgba(0, 0, 0, .8)';
+    ctx.fillRect(0, canvas.height / 2, canvas.width, canvas.height / 2);
+  }
 
   app.init();
   initAnimation();
+
 }());
